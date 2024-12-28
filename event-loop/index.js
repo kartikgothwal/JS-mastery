33024e9b9a4b5c25d41af73250ad53580a43491b
Kartik Gothwal <kartikgothwal28@gmail.com>
2024-12-28 19:49:33 +0530

    Event loop

---
+++ event-loop/index.js
@@ -0,0 +1,20 @@
+// Link for Diagram =https://excalidraw.com/#json=HncRKbVUYl3pfGScaefXs,it2VgPy0v0q1TcFqNkrRkA
+//  console.log("Hello world")
+// setTimeout(()=>{
+//     console.log("Timeout")
+// })
+// console.log("End");
+
+debugger
+ console.log("Hello world")
+setTimeout(()=>{
+    console.log("Timeout")
+})
+async function getData(){
+   fetch('https://jsonplaceholder.typicode.com/todos/30').then((res)=>res.json()).then((data)=>{
+    console.log("Data", data)
+   });
+    
+}
+getData()
+console.log("End");
